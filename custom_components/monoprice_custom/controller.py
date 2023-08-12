3b2150b5046ebcae8fd557500d4f4f0d4678300e
Jorge Beserra <5083403+JorgeBeserra@users.noreply.github.com>
2023-08-12 12:13:29 -0300

    Update controller.py

--- custom_components/monoprice_custom/controller.py
+++ custom_components/monoprice_custom/controller.py
@@ -1,12 +1,6 @@
 from abc import ABC, abstractmethod
-from base64 import b64encode
-import binascii
-import requests
-import logging
 import json
-
-from homeassistant.const import ATTR_ENTITY_ID
-#from . import Helper
+import logging
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -52,28 +46,33 @@ class AbstractController(ABC):
         """Send a command."""
         pass
 
+
 class MQTTController(AbstractController):
     """Controls a MQTT device."""
 
     def check_encoding(self, encoding):
         """Check if the encoding is supported by the controller."""
         if encoding not in MQTT_COMMANDS_ENCODING:
-            raise Exception("The encoding is not supported "
-                            "by the mqtt controller.")
+            raise Exception("The encoding is not supported by the MQTT controller.")
 
     async def send(self, command, level: int):
         """Send a command."""
-
         service_data = {
-            'topic': 'cmnd/' + self._unique_id + '/driver129',
-            'payload': command + ' ' + level
+            'topic': f'cmnd/{self.unique_id}/driver129',
+            'payload': f'{command} {level}'
         }
 
-        await self.hass.services.async_call(
-            'mqtt', 'publish', service_data)
-    
+        try:
+            await self.hass.services.async_call('mqtt', 'publish', service_data)
+        except Exception as e:
+            _LOGGER.error("Error sending MQTT command: %s", e)
+
     async def exist(self):
-        return self.hass.services.has_service('mqtt', 'publish')
+        try:
+            return self.hass.services.has_service('mqtt', 'publish')
+        except Exception as e:
+            _LOGGER.error("Error checking MQTT service existence: %s", e)
+
 
 class ESPHomeController(AbstractController):
     """Controls a ESPHome device."""
@@ -81,15 +80,19 @@ class ESPHomeController(AbstractController):
     def check_encoding(self, encoding):
         """Check if the encoding is supported by the controller."""
         if encoding not in ESPHOME_COMMANDS_ENCODING:
-            raise Exception("The encoding is not supported "
-                            "by the ESPHome controller.")
+            raise Exception("The encoding is not supported by the ESPHome controller.")
 
     async def send(self, command, level: int):
         """Send a command."""
-        service_data = {'command':  json.loads(command) + ' ' + json.loads(command) }
+        service_data = {'command': json.loads(command) + ' ' + json.loads(command)}
 
-        await self.hass.services.async_call(
-            'esphome', self._controller_data, service_data)
-    
+        try:
+            await self.hass.services.async_call('esphome', self._controller_data, service_data)
+        except Exception as e:
+            _LOGGER.error("Error sending ESPHome command: %s", e)
+
     async def exist(self):
-        return self.hass.services.has_service('esphome', self._controller_data)
+        try:
+            return self.hass.services.has_service('esphome', self._controller_data)
+        except Exception as e:
+            _LOGGER.error("Error checking ESPHome service existence: %s", e)
